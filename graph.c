dd7b91db4092c8f3394cfeff889a0ece34906b65
Eduardo Silva <eduardosg.my@gmail.com>
2025-02-08 13:31:31 -0300

    Create and implement graph library. Dijkstra to do.

---
+++ graph.c
@@ -0,0 +1,100 @@
+#include "graph.h"
+#include "colors.h"
+
+int size_error(int src, int dst, int size) {
+
+    if(src >= size || dst >= size) {
+        printf("%sERROR: Edge of %d to %d could not be inserted. Source or destiny node is bigger than graph's size\n%s",
+        RED, src, dst, RESET);
+        return 1;
+    }
+    else if(src < 0 || dst < 0) {
+        printf("%sERROR: Edge of %d to %d could not be inserted. Source or destiny node is smaller than graph's size\n%s",
+        RED, src, dst, RESET);
+        return 1;  
+    }
+
+return 0;
+}
+
+struct node {
+    int id;
+    float dist;
+    int prt;
+};
+
+struct graph {
+    int size;
+    float ** matrix;
+};
+
+graph * create_graph(int size) {
+
+    if(size <= 0) {
+        printf("%sERROR: Informed size to create graph is 0 or smaller\n%s",
+        RED, RESET);
+        return NULL;
+    }
+    graph * g = (graph *) calloc(1, sizeof(graph));
+
+    g -> size = size;
+    g -> matrix = (float **) calloc(size, sizeof(float *));
+
+    for(int i = 0; i < size; i++) {
+        g -> matrix[i] = (float *) calloc(size, sizeof(float));
+    }
+
+return g;
+}
+
+void insert_edge(graph * g, int src, int dst, float w) {
+
+    if(size_error(src, dst, g -> size)) return;
+    g -> matrix[src][dst] = w;
+
+}
+
+void remove_edge(graph * g, int src, int dst) {
+
+    if(size_error(src, dst, g -> size)) return;
+    g -> matrix[src][dst] = 0;
+
+}
+
+void print_graph(graph * g) {
+
+    for(int i = 0; i < g -> size; i++) {
+        for(int j = 0; j < g -> size; j++) {
+            float v = g -> matrix[i][j];
+            if(v == 0) printf("%.1f ", v);
+            else printf("%s%.1f %s", CYAN, v, RESET);
+        }
+        printf("\n");
+    }
+
+}
+
+void free_graph(graph * g) {
+
+    for(int i = 0; i < g -> size; i++) {
+        free(g -> matrix[i]);
+    }
+    free(g -> matrix);
+    free(g);
+
+}
+
+node ** dijkstra(graph * g, int src) {
+
+    //implement dijkstra!!
+
+}
+
+void free_nodes(node ** n, int size) {
+
+    for(int i = 0; i < size; i++) {
+        free(n[i]);
+    }
+    free(n);
+
+}
---
+++ graph.h
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node node;
+
+typedef struct graph graph;
+
+/**
+ * Creates a graph
+ * @param size graph's size
+ * @return graph pointer
+ */
+graph * create_graph(int size);
+
+/**
+ * Inserts a new edge or modifies an already existing one
+ * @param g graph to add the edge
+ * @param src source node
+ * @param dst destiny node
+ * @param w edge's weight
+ */
+void insert_edge(graph * g, int src, int dst, float w);
+
+/**
+ * Turns the value of the edge to 0
+ * @param g graph to remove the edge
+ * @param src source node
+ * @param dst destiny node
+ */
+void remove_edge(graph * g, int src, int dst);
+
+/**
+ * Prints a graph
+ * @param g graph to print
+ */
+void print_graph(graph * g);
+
+/**
+ * Frees a graph
+ * @param g graph to free
+ */
+void free_graph(graph * g);
+
+/**
+ * Finds the shortest path from a node to all the other ones
+ * @param g graph to analyze
+ * @param src source node
+ * @return a node array with all the paths unsorted
+ */
+node ** dijkstra(graph * g, int src);
+
+/**
+ * Free the nodes array from Dijkstra function
+ * @param node array of nodes
+ * @param size size of the array
+ */
+void free_nodes(node ** n, int size);
